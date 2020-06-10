3523d4a67c873f06e1039afc9667a514c52d3b45
Vito <vitof970@gmail.com>
2020-06-10 11:51:41 +0200

    Implemented result.csv and bibtex generator

--- Paper.py
+++ Paper.py
@@ -8,25 +8,26 @@ Created on Mon Jun  8 21:43:30 2020
 
 class Paper:
     
-    def __init__(self,title):
-        self.crs_title = title
+    def __init__(self,title, sc_link):
+        self.sc_title = title
+        self.sc_link = sc_link
         
         self.crs_title = None
         self.crs_DOI = None
         self.crs_authors = []
         self.crs_bibtex = None
         
-        self.sc_title = None
-        self.sc_link = None
-        
         self.downloaded = False
         self.crossref_found = False
+        self.bibtex_found = False
         
         
     
     def setAuthors(self,authors):
         for a in authors:
-            self.crs_authors.append((a["given"],a["family"]))
+            name = a["given"] if "given" in a else  "None"
+            surname = a["family"] if "family" in a else  "None"
+            self.crs_authors.append((name,surname))
             
     def getFileName(self):
         fname = ""
@@ -39,3 +40,51 @@ class Paper:
             fname += year
         
         return fname + ".pdf"
+    
+    def setBibtex(self,bibtex):
+        if bibtex!=None and len(bibtex)>7 and bibtex[:8]=="@article" :
+            self.crs_bibtex = bibtex
+            self.bibtex_found = True
+            
+    def canBeDownloaded(self):
+        if self.crs_DOI!=None or self.sc_link!=None:
+            return True
+        else:
+            return False
+            
+    def reset(self):
+        self.crs_title = None
+        self.crs_DOI = None
+        self.crs_authors = []
+        self.crs_bibtex = None
+        
+        self.downloaded = False
+        self.crossref_found = False
+        self.bibtex_found = False
+    
+    def generateReport(papers, path):
+        content = "SC Name,CRS Name,Downloaded,SC Link,CRS DOI,Bibtex"
+        for list_p in papers:
+            for p in list_p:  
+                content += ("\n"+str(p.sc_title)+","+str(p.crs_title)+","+str(p.downloaded)+
+                ","+str(p.sc_link)+","+str(p.crs_DOI)+","+str(p.bibtex_found))
+           
+        f = open(path, "w")
+        f.write(content)
+        f.close()
+        
+        
+    def generateBibtex(papers, path):
+        content = ""
+        for list_p in papers:
+            for p in list_p:  
+                if p.crossref_found:
+                    note =  ",\n\tnote = {\doi{" + str(p.crs_DOI) + "}}\n}\n\n"
+                    bibtex = p.crs_bibtex[:(len(p.crs_bibtex)-2)] + note
+                    content += bibtex
+           
+        f = open(path, "w")
+        f.write(content)
+        f.close()
+        
+            
--- PyPaperBot.py
+++ PyPaperBot.py
@@ -34,11 +34,12 @@ def slugify(value):
     return value
         
 
-def main(query, number, dwn_dir, proxies):
+def main(query, number, dwn_dir):
     
     HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
     url = "https://scholar.google.com/scholar?q="+query+"&as_vis=1&as_sdt=1,5";
     
+    papers_result = []    
 
     for i in range(0,int(number/10)):
         if i>0:
@@ -56,54 +57,63 @@ def main(query, number, dwn_dir, proxies):
                 info_valids += 1
         print("Papers info from Crossref: "+str(info_valids))
      
-        SciHubDownload(papersInfo, dwn_dir, proxies)
+        papers_result.append(papersInfo)
+        SciHubDownload(papersInfo, dwn_dir)
         
         print("Next -> ",i+1)
         
+    Paper.generateReport(papers_result,dwn_dir+"result.csv")
+    Paper.generateBibtex(papers_result,dwn_dir+"bibtex.txt")
         
-def SciHubDownload(papers, dwnl_dir, proxies):
+        
+def SciHubDownload(papers, dwnl_dir):
     HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
-
-    SciHub_URLs = ["https://sci-hub.tw/"]
+    SciHub_URL = "https://sci-hub.tw/"
     
-    for p in papers:
-        pdf_dir = dwnl_dir + p.getFileName()
-        print("Downloading -> "+str(p.getFileName()))
-        
-                    
-        i = 0;
-        use_sc_link = False
-        while p.downloaded==False and i<len(SciHub_URLs):        
+    for p in papers:      
+        if p.canBeDownloaded():        
+            pdf_dir = dwnl_dir + p.getFileName()
+            print("Downloading -> "+str(p.getFileName()))
             
-            try:   
-                if use_sc_link==False and p.crs_DOI!=None:
-                    url = SciHub_URLs[i]+p.crs_DOI
-                else:
-                    url = SciHub_URLs[i]+p.sc_link
-                
-                
-                r = requests.get(url, headers=HEADERS)
-                pdf_link = HTMLparsers.getSchiHubPDF(r.text)
-                
-                time.sleep(random.randint(1,5))
-                
-                if(pdf_link != None):
-                    r2 = requests.get(pdf_link, headers=HEADERS)
-                    with open(pdf_dir, 'wb') as f:
-                        f.write(r2.content)
-                        p.downloaded = True
-            except:
-                pass
-                
-            i = i + 1
-            if p.downloaded==False and i==len(SciHub_URLs) and use_sc_link==False and p.crs_DOI!=None:
-                i = 0
-                use_sc_link = True
+                        
+            use_sc_link = False
+            doi_used = False
+            errors = 0
+            while p.downloaded==False and use_sc_link==False and errors!=2:        
+                try:   
+                    if doi_used==False and use_sc_link==False and p.crs_DOI!=None:
+                        url = SciHub_URL + p.crs_DOI
+                        doi_used = True
+                    else:
+                        url = SciHub_URL + p.sc_link
+                        use_sc_link = True
+                    
+                    
+                    r = requests.get(url, headers=HEADERS)
+                    pdf_link = HTMLparsers.getSchiHubPDF(r.text)
+                    
+                    time.sleep(random.randint(1,5))
+                    
+                    if(pdf_link != None):
+                        r2 = requests.get(pdf_link, headers=HEADERS)
+                        with open(pdf_dir, 'wb') as f:
+                            f.write(r2.content)
+                            p.downloaded = True
+                except:
+                    errors +=1
                     
         
     
 def similarStrings(a, b):
     return SequenceMatcher(None, a, b).ratio()
+
+
+def getTimestamp(paper):
+    timestamp = 0
+    if "deposited" in paper:
+        if "timestamp" in paper["deposited"]:
+            timestamp = int(paper["deposited"]["timestamp"])
+    return timestamp
 
 
 def getPapersInfo(papers):
@@ -113,13 +123,18 @@ def getPapersInfo(papers):
         queries = {'query.bibliographic': title,'sort':'relevance',"select":"title,DOI,author"}
 
         found = False;
-        paper_found = Paper(title)
-        paper_found.sc_link = paper[1]
+        found_timestamp = 0
+        paper_found = Paper(title,paper[1])
         for el in iterate_publications_as_json(max_results=30, queries=queries):
+           
+            el_date = getTimestamp(el);
             
-            if found==False and ("title" in el) and similarStrings(title ,el["title"][0].strip().lower())>0.75:
+            if (found==False or el_date>found_timestamp) and ("title" in el) and similarStrings(title ,el["title"][0].strip().lower())>0.75:
+                found_timestamp = el_date
+                paper_found.reset()
+                
                 if "title" in el:
-                    paper_found.sc_title = el["title"][0].strip().lower()
+                    paper_found.crs_title = el["title"][0].strip().lower()
                 if "DOI" in el:
                     paper_found.crs_DOI = el["DOI"].strip().lower()
                 if "author" in el:
@@ -128,17 +143,15 @@ def getPapersInfo(papers):
                 try: 
                     url_bibtex = "http://api.crossref.org/works/" + paper_found.crs_DOI + "/transform/application/x-bibtex"
                     x = requests.get(url_bibtex)
-                    paper_found.crs_bibtex = x.text
+                    paper_found.setBibtex(str(x.text))
                 except:
                     pass
  
                 found = True
-                paper_found.crossref_found = True
-                papers_return.append(paper_found)
+                paper_found.crossref_found = True 
         
             
-        if found == False:
-            print("NOT FOUND -> "+title)
+        papers_return.append(paper_found)
                 
         time.sleep(random.randint(1,5))
         
@@ -147,7 +160,6 @@ def getPapersInfo(papers):
     
 if __name__ == "__main__":
     query = "text analysis for accounting"
-    paper_num = 100
+    paper_num = 10
     dwn_dir = "E:/Users/Vito/Desktop/testPaperbot/"
-    proxies = ["https://107.191.41.188:8080","https://142.93.117.211:3128","https://209.141.49.11:8080"]
-    main(query, paper_num, dwn_dir, proxies)
+    main(query, paper_num, dwn_dir)
