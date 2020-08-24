6c46ad057098c59cb2746bb98bb4c7f99f3edf16
ferru97 <vitof970@gmail.com>
2020-08-24 19:43:06 +0200

    Fix pypi classifiers

--- setup.py
+++ setup.py
@@ -40,8 +40,8 @@ setup(
       ],
   classifiers=[
     'Development Status :: 4 - Beta',
-    'Intended Audience :: Researchers',
-    'Topic :: Research  papers :: Download Tools',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
     'License :: OSI Approved :: MIT License',  
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
