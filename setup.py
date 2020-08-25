7f91ed788c9f645bfddcca383cf759b1f892be98
ferru97 <vitof970@gmail.com>
2020-08-25 10:30:06 +0200

    Update donwload URL for PyPI

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
   author = 'Vito Ferrulli',
   author_email = 'vitof970@gmail.com',
   url = 'https://github.com/ferru97/PyPaperBot',
-  download_url = 'https://github.com/ferru97/PyPaperBot/archive/v0.9.5-beta.tar.gz',
+  download_url = 'https://github.com/ferru97/PyPaperBot/archive/v0.9.5.1-beta.tar.gz',
   keywords = ['download-papers','google-scholar', 'scihub', 'scholar', 'crossref', 'papers'],
   install_requires=[          
         'astroid==2.4.2',
