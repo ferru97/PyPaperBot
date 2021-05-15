66c52763052e3a67e615fdbab21722d2d39f99a9
suhan-paradkar <12suhangp34@gmail.com>
2021-05-15 13:03:43 +0530

    remove few errors

--- PyPaperBot/proxy.py
+++ PyPaperBot/proxy.py
@@ -1,8 +1,8 @@
 import proxy
 
 def proxy(host, port):
-if __name__ == '__main__':
-proxy.main([
+    if __name__ == '__main__':
+        proxy.main([
 	'--hostname', host,
 	'--port', port
-])
+    ])
